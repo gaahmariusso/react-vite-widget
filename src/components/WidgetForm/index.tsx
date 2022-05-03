f722c1703b9d5c541008ce48938716a26f0e9295
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-05-03 12:54:25 -0300

    add components FeedbackContentStep and FeedbackTypeStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,8 +5,10 @@ import { CloseButton } from "../CloseButton";
 import bugImageUrl from '../../assets/images/bug.svg';
 import ideaImageUrl from '../../assets/images/idea.svg';
 import thoughtImageUrl from '../../assets/images/thought.svg';
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -30,42 +32,31 @@ const feedbackTypes = {
     },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[cal(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-
-                <CloseButton />
-            </header>
 
             {!feedbackType ? (
-                <div className="flex py-8 gap-2 w-full">
-                    {Object.entries(feedbackTypes).map(([key, value]) => {
-                        return (
-                            <button
-                                key={key}
-                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                                onClick={() => setFeedbackType(key as FeedbackType)}
-                                type="button"
-                            >
-                                <img src={value.image.source} alt={value.image.alt} />
-                                <span>{value.title}</span>
-                            </button>
-                        )
-                    })}
-                </div>
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>Hello World</p>
-            )}
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
+            )
+            }
 
             <footer className="text-xs text-neutral-400">
                 Feito com ❤️ por <a className="underline underline-offset-2" href="https://github.com/gaahmariusso">Gabriel Mariusso</a>
             </footer>
-        </div>
+        </div >
     );
 }
