29757a6d5e41831eabdd5c66446c9b8a4b3cf09d
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-05-03 19:41:05 -0300

    add Screenshot | FeedbackSuccessStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -7,6 +7,7 @@ import ideaImageUrl from '../../assets/images/idea.svg';
 import thoughtImageUrl from '../../assets/images/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -36,23 +37,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false)
 
     function handleRestartFeedback() {
+        setFeedbackSent(false);
         setFeedbackType(null);
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[cal(100vw-2rem)] md:w-auto">
 
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            {feedbackSent ? (
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-                <FeedbackContentStep
-                    feedbackType={feedbackType}
-                    onFeedbackRestartRequested={handleRestartFeedback}
-                />
-            )
-            }
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ❤️ por <a className="underline underline-offset-2" href="https://github.com/gaahmariusso">Gabriel Mariusso</a>
